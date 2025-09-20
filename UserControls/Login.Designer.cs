82e5c9a40bf0497645ebaa08cb82453c2c72a77b
Suraj Jadhav <surajj4837@gmail.com>
2025-09-20 17:52:19 +0530

    Added login user control. Added language setting functions.

---
+++ UserControls/Login.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace HardwareRentalApp.UserControls
+{
+    partial class Login
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            pnl_login = new Panel();
+            btn_login = new Button();
+            tb_Password = new TextBox();
+            tb_MobileNo = new TextBox();
+            lbl_Password = new Label();
+            lbl_MobileNo = new Label();
+            ll_register = new LinkLabel();
+            lbl_admin = new Label();
+            cb_Language = new ComboBox();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            lbl_Language = new Label();
+            pnl_login.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnl_login
+            // 
+            pnl_login.BorderStyle = BorderStyle.FixedSingle;
+            pnl_login.Controls.Add(btn_login);
+            pnl_login.Controls.Add(tb_Password);
+            pnl_login.Controls.Add(tb_MobileNo);
+            pnl_login.Controls.Add(lbl_Password);
+            pnl_login.Controls.Add(lbl_MobileNo);
+            resources.ApplyResources(pnl_login, "pnl_login");
+            pnl_login.Name = "pnl_login";
+            // 
+            // btn_login
+            // 
+            resources.ApplyResources(btn_login, "btn_login");
+            btn_login.Name = "btn_login";
+            btn_login.UseVisualStyleBackColor = true;
+            btn_login.Click += btn_login_Click;
+            // 
+            // tb_Password
+            // 
+            resources.ApplyResources(tb_Password, "tb_Password");
+            tb_Password.Name = "tb_Password";
+            tb_Password.UseSystemPasswordChar = true;
+            // 
+            // tb_MobileNo
+            // 
+            resources.ApplyResources(tb_MobileNo, "tb_MobileNo");
+            tb_MobileNo.Name = "tb_MobileNo";
+            // 
+            // lbl_Password
+            // 
+            resources.ApplyResources(lbl_Password, "lbl_Password");
+            lbl_Password.Name = "lbl_Password";
+            // 
+            // lbl_MobileNo
+            // 
+            resources.ApplyResources(lbl_MobileNo, "lbl_MobileNo");
+            lbl_MobileNo.Name = "lbl_MobileNo";
+            // 
+            // ll_register
+            // 
+            resources.ApplyResources(ll_register, "ll_register");
+            ll_register.Name = "ll_register";
+            ll_register.TabStop = true;
+            ll_register.LinkClicked += ll_register_LinkClicked;
+            // 
+            // lbl_admin
+            // 
+            resources.ApplyResources(lbl_admin, "lbl_admin");
+            lbl_admin.BackColor = Color.LightGreen;
+            lbl_admin.Name = "lbl_admin";
+            // 
+            // cb_Language
+            // 
+            resources.ApplyResources(cb_Language, "cb_Language");
+            cb_Language.DropDownStyle = ComboBoxStyle.DropDownList;
+            cb_Language.FormattingEnabled = true;
+            cb_Language.Items.AddRange(new object[] { resources.GetString("cb_Language.Items"), resources.GetString("cb_Language.Items1") });
+            cb_Language.Name = "cb_Language";
+            cb_Language.SelectedIndexChanged += Language_SelectedIndexChanged;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(tableLayoutPanel1, "tableLayoutPanel1");
+            tableLayoutPanel1.Controls.Add(lbl_Language, 0, 0);
+            tableLayoutPanel1.Controls.Add(cb_Language, 1, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // lbl_Language
+            // 
+            resources.ApplyResources(lbl_Language, "lbl_Language");
+            lbl_Language.Name = "lbl_Language";
+            // 
+            // Login
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            Controls.Add(pnl_login);
+            Controls.Add(ll_register);
+            Controls.Add(lbl_admin);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Login";
+            pnl_login.ResumeLayout(false);
+            pnl_login.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_login;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.TextBox tb_MobileNo;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Label lbl_MobileNo;
+        private System.Windows.Forms.LinkLabel ll_register;
+        private System.Windows.Forms.Label lbl_admin;
+        private System.Windows.Forms.ComboBox cb_Language;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lbl_Language;
+    }
+}
---
+++ UserControls/Login.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace HardwareRentalApp.UserControls
+{
+    public partial class Login : UserControl
+    {
+        //private DatabaseInterface DBInterface = new DatabaseInterface();
+        private ResourceManager LangManager = new ResourceManager("HardwareRentalApp.Resources.MessageFiles.MessageStrings", typeof(Login).Assembly);
+        //private DataTable dtUser = new DataTable();
+        //public static Int32 AdminID;
+        //public static string m_st_Category;
+        //private ActivateProduct Obj_ProductActivation = new ActivateProduct();
+        string culture = Properties.Settings.Default.Language;
+
+        public Login()
+        {
+            InitializeComponent();
+
+            SetLanguage();
+
+            ApplyLanguage();
+        }
+        public void ApplyLanguage()
+        {
+            this.lbl_admin.Text = LangManager.GetString("AdminLogin");
+            this.lbl_MobileNo.Text = LangManager.GetString("MobileNumber");
+            this.lbl_Password.Text = LangManager.GetString("Password");
+            this.lbl_Language.Text = LangManager.GetString("Language");
+            this.btn_login.Text = LangManager.GetString("Login");
+            this.ll_register.Text = LangManager.GetString("RegisterHere");
+        }
+
+        private void SetLanguage()
+        {
+            if (Properties.Settings.Default.Language == "mr-IN")
+                cb_Language.SelectedIndex = 0;
+            else if (Properties.Settings.Default.Language == "mr")
+                cb_Language.SelectedIndex = 0;
+            else if (Properties.Settings.Default.Language == "en")
+                cb_Language.SelectedIndex = 1;
+            else
+                cb_Language.SelectedIndex = 1;      //Default English language
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            //    string message = "";
+
+            //    if (true)//(Obj_ProductActivation.VerifyProduct(out message))  //Temporarily deactivated
+            //    {
+            //        string mobile_num = tb_MobileNo.Text;
+            //        string password = tb_Password.Text;
+
+            //        if (mobile_num.Equals(""))
+            //        {
+            //            MessageBox.Show(LangManager.GetString("EnterUsername"));
+            //        }
+            //        else if (password.Equals(""))
+
+            //        {
+            //            MessageBox.Show(LangManager.GetString("EnterPassword"));
+            //        }
+            //        else
+            //        {
+            //            try
+            //            {
+            //                byte[] encryptedPassword_byte = EncryptionDecryption.EncryptData(password);
+            //                string encryptedPassword_str = Convert.ToBase64String(encryptedPassword_byte);
+
+            //                //byte[] byteArray = Convert.FromBase64String(encryptedPassword_str);
+
+            //                string query = "Select * from Admin Where mobile_number = @mobile_num AND password = @encryptedPassword_str";
+
+            //                var parameters = new Dictionary<string, object> { { "@mobile_num", mobile_num }, { "@encryptedPassword_str", encryptedPassword_str } };
+
+            //                DBInterface.ReadDataThroughAdapter(query, dtUser, parameters);
+
+            //                if (dtUser.Rows.Count >= 1)
+            //                {
+            //                    AdminID = Convert.ToInt32(dtUser.Rows[0]["ID"].ToString());
+
+            //                    m_st_Category = dtUser.Rows[0]["category"].ToString();
+
+            //                    if (m_st_Category == "Master Admin")
+            //                    {
+            //                        GlobalState.e_LoginProfile = Profile.Master_Admin;
+            //                    }
+            //                    else if (m_st_Category == "Admin")
+            //                    {
+            //                        GlobalState.e_LoginProfile = Profile.Admin;
+            //                    }
+            //                    else if (m_st_Category == "Support")
+            //                    {
+            //                        GlobalState.e_LoginProfile = Profile.Support;
+            //                    }
+
+            //                    //if (Obj_ProductActivation.LessActivationDaysRemaining())  //Temporarily deactivated
+            //                    //{
+            //                    //    MessageBox.Show(LangManager.GetString("ActivationNeeded"));
+            //                    //}
+
+            //                    // Save selected language
+            //                    Properties.Settings.Default.Language = culture;
+            //                    Properties.Settings.Default.Save();
+
+            //                    //Open Home page.
+            //                    Homepage homepage = this.ParentForm as Homepage;
+            //                    if (homepage != null)
+            //                    {
+            //                        homepage.ApplyLanguage();
+            //                        homepage.OpenChildControl(new HardwareRentalApp.UserControls.Home());
+            //                    }
+            //                }
+            //                else
+            //                {
+            //                    MessageBox.Show(LangManager.GetString("InvalidCredentials"));
+            //                }
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                // Show exception details in a MessageBox
+            //                MessageBox.Show(
+            //                    ex.Message,           // The exception message
+            //                    "Error",              // Title of the message box
+            //                    MessageBoxButtons.OK, // MessageBox buttons (OK)
+            //                    MessageBoxIcon.Error);// MessageBox icon (Error)
+            //            }
+            //        }
+            //    }
+            //    else
+            //    {
+            //        ShowProductActivation(message);
+            //    }
+        }
+        //private void ShowProductActivation(string message)
+        //{
+        //    using (var PA_form = new ActivateProduct(message))
+        //    {
+        //        if (PA_form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+        //        {
+        //        }
+        //    }
+        //}
+
+        private void ll_register_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //var RegisterAdminForm = new RegisterAdmin();
+            //RegisterAdminForm.ShowDialog();
+        }
+
+        private void Language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cb_Language.SelectedIndex)
+            {
+                case 0:
+                    culture = "mr-IN";
+                    break;
+
+                case 1:
+                    culture = "en";
+                    break;
+            }
+
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(culture);
+
+            ApplyResourceToControl(this,
+            new ComponentResourceManager(typeof(Login)),
+            new CultureInfo(culture));
+
+            ApplyLanguage();
+        }
+        private void ApplyResourceToControl(Control control, ComponentResourceManager cmp, CultureInfo cultureInfo)
+        {
+            foreach (Control child in control.Controls)
+            {
+                //Store current position and size of the control
+                var childSize = child.Size;
+                var childLoc = child.Location;
+
+                //Apply CultureInfo to child control
+                ApplyResourceToControl(child, cmp, cultureInfo);
+
+                //Restore position and size
+                child.Location = childLoc;
+                child.Size = childSize;
+            }
+
+            //Do the same with the parent control
+            var parentSize = control.Size;
+            var parentLoc = control.Location;
+            cmp.ApplyResources(control, control.Name, cultureInfo);
+            control.Location = parentLoc;
+            control.Size = parentSize;
+        }
+    }
+}
