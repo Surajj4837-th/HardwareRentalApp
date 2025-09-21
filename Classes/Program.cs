7e0f5bdca70c8a446cb87fe752b119f99eb1a6a3
Suraj Jadhav <surajj4837@gmail.com>
2025-09-21 12:55:26 +0530

    Moved this file.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,7 +1,18 @@
-namespace HardwareRentalApp
+namespace HardwareRentalApp.Classes
 {
     internal static class Program
     {
+        public enum Profile
+        {
+            Owner,
+            Employee
+        }
+
+        public static class GlobalState
+        {
+            public static Profile e_LoginProfile;
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
