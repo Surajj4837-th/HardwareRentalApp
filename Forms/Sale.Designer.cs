d5d5fa211e7cf99e612ceda97b5bca2a6fe4cff7
Suraj Jadhav <surajj4837@gmail.com>
2025-09-23 05:57:38 +0530

    Added sale form. Added functin to access the items and update the names as per current culture.

---
+++ Forms/Sale.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace HardwareRentalApp.Forms
+{
+    partial class Sale
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sale));
+            btn_Close = new Button();
+            SuspendLayout();
+            // 
+            // btn_Close
+            // 
+            btn_Close.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btn_Close.FlatAppearance.BorderSize = 0;
+            btn_Close.FlatStyle = FlatStyle.Flat;
+            btn_Close.Font = new Font("Nirmala UI", 8.25F);
+            btn_Close.Image = (Image)resources.GetObject("btn_Close.Image");
+            btn_Close.ImeMode = ImeMode.NoControl;
+            btn_Close.Location = new Point(986, 2);
+            btn_Close.Margin = new Padding(1);
+            btn_Close.Name = "btn_Close";
+            btn_Close.Size = new Size(33, 33);
+            btn_Close.TabIndex = 3;
+            btn_Close.UseVisualStyleBackColor = true;
+            btn_Close.Click += btn_Close_Click;
+            // 
+            // Sale
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 1066);
+            ControlBox = false;
+            Controls.Add(btn_Close);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Sale";
+            Text = "Sale";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn_Close;
+    }
+}
---
+++ Forms/Sale.cs
@@ -0,0 +1,62 @@
+﻿using HardwareRentalApp.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HardwareRentalApp.Forms
+{
+    public partial class Sale : Form
+    {
+        private ResourceManager LangManager = new ResourceManager("HardwareRentalApp.Resources.MessageFiles.MessageStrings", typeof(Sale).Assembly);
+        private DBInterface obj_DBAccess = new DBInterface();
+
+        public Sale()
+        {
+            InitializeComponent();
+        }
+
+        public List<Items> GetLocalizedItems()
+        {
+            string query = "SELECT ItemId, ItemKey, Price FROM Items";
+
+            var items = obj_DBAccess.ExecuteQuery(query, reader => new Items
+            {
+                ItemId = reader.GetInt32(0),
+                ItemKey = reader.GetString(1),
+                Price = reader.GetDecimal(2)
+            });
+
+            LocalizeItems(items);
+            return items;
+        }
+
+        private void LocalizeItems(List<Items> items)
+        {
+            for (int i = 0; i < items.Count; i++)
+            {
+                string localizedName = LangManager.GetString(
+                    items[i].ItemKey,
+                    Thread.CurrentThread.CurrentUICulture
+                );
+
+                var item = items[i];
+                item.LocalizedName = string.IsNullOrEmpty(localizedName)
+                                        ? item.ItemKey
+                                        : localizedName;
+                items[i] = item;
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
