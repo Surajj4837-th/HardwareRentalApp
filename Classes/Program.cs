5716f4ec1054eeae02525fde0f7fc78cbf782edf
Suraj Jadhav <surajj4837@gmail.com>
2025-10-08 05:28:25 +0530

    Added structure for bill summary.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -20,6 +20,15 @@ namespace HardwareRentalApp.Classes
         public string LocalizedName { get; set; }
         public decimal Rent { get; set; }
     }
+    public class BillItemSummary
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
+    }
+
 
     public struct BillSummary
     {
