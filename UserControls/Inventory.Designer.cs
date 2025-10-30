87fb2218aba7a172d657f182cee391abfa8ad538
Suraj Jadhav <surajj4837@gmail.com>
2025-10-31 05:05:15 +0530

    Added inventory user control.

---
+++ UserControls/Inventory.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace HardwareRentalApp.UserControls
+{
+    partial class Inventory
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Inventory
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Font = new Font("Nirmala UI", 13.8F);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Inventory";
+            Size = new Size(1486, 962);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
