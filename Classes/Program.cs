6210a5556a4ab3d232177234fcfe34b7240f1446
Suraj Jadhav <surajj4837@gmail.com>
2025-11-09 18:26:33 +0530

    Updated variable names.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -20,7 +20,7 @@ namespace HardwareRentalApp.Classes
         public int ItemId { get; set; }
         public string ItemName { get; set; }
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
+        public decimal Rent { get; set; }
         public decimal Total { get; set; }
     }
 
